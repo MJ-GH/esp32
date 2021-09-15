941c41e4f034077c08749c9a6cc416df1e964119
Martin Jensen <mart924p@elev.tec.dk>
2021-09-15 11:28:53 +0200

    update

--- esp32.ino
+++ esp32.ino
@@ -1,41 +1,63 @@
+
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <WiFi.h>
 #include <Wire.h>
 
-#define ds1621 0x90 >> 1                        // shift required by wire.h
+#define ds1621 0x90 >> 1
+
+const char* ssid = "ESP32 SSID";
+const char* password = "password";
+
+AsyncWebServer server(80);                    // Creates an AsyncWebServer object on port 80.
 
 void setup()
 {
   Serial.begin(115200);
 
   Wire.begin();
-  Wire.beginTransmission(ds1621);             // connect to DS1621 (#0)
-  Wire.write(0xAC);                            // Access Config
-  Wire.write(0x02);                            // set for continuous conversion
-  Wire.beginTransmission(ds1621);             // restart
-  Wire.write(0xEE);                            // start conversions
+  Wire.beginTransmission(ds1621);             // Connect to DS1621
+  Wire.write(0xAC);                           // Access config
+  Wire.write(0x02);                           // Set for continuous conversion
+  Wire.beginTransmission(ds1621);             // Restart
+  Wire.write(0xEE);                           // Start conversions
   Wire.endTransmission();
-}
 
+  WiFi.mode(WIFI_AP);                         // Configures the ESP32 as a soft Access Point, "soft" because it doesn't connect further to a wired network like a router.
+  WiFi.softAP(ssid, password);                // "ssid" is the name of the ESP32, "password" is the password required to connect to the AP. Set to NULL if no password is required.
+
+  server.on("/temp", HTTP_GET, [](AsyncWebServerRequest * req) {
+    req->send(200, "text/plain", String(readTemp()));
+  });
+
+  server.begin();                             // Starts the server
+}
 
 void loop()
 {
+  Serial.println(readTemp());
+}
+
+float readTemp() {
+
   int8_t firstByte;
   int8_t secondByte;
   float temp = 0;
 
-  delay(1000);                                // give time for measurement
+  delay(1000);                                // Give time for measurement
 
   Wire.beginTransmission(ds1621);
-  Wire.write(0xAA);                            // read temperature command
+  Wire.write(0xAA);                           // Read temperature
   Wire.endTransmission();
-  Wire.requestFrom(ds1621, 2);    // request two bytes from DS1621 (0.5 deg. resolution)
+  Wire.requestFrom(ds1621, 2);                // Request two bytes from DS1621 (0.5 deg. resolution)
 
-  firstByte = Wire.read();           // get first byte
-  secondByte = Wire.read();    // get second byte
+  firstByte = Wire.read();                    // Get first byte
+  secondByte = Wire.read();                   // Get second byte
 
   temp = firstByte;
 
-  if (secondByte)             // if there is a 0.5 deg difference
+  if (secondByte)                             // If there is a 0.5 deg. difference
     temp += 0.5;
 
-  Serial.println(temp);
+  return temp;
 }
